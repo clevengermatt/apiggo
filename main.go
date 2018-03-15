7ef1c9a4ddcfa32b0fe53418d95c62be8820ccee
mattcdmob <dmob-matt@Matts-MacBook-Pro.local>
2018-03-15 18:13:05 -0400

    Init for apiggo

---
+++ main.go
@@ -0,0 +1,66 @@
+package apiggo
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
+// Handler turns an APIGatewayProxyRequest into a standard http.Request
+func Handler(handler http.Handler, host string, proxyRequest events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// Create a new *http.Request and *httptest.ResponseRecorder
+	r := httptest.NewRequest(proxyRequest.HTTPMethod, proxyRequest.Path, strings.NewReader(proxyRequest.Body))
+	w := httptest.NewRecorder()
+
+	// Set the host to whatever your APIG base path is
+	r.Host = host
+
+	// Add the APIGatewayProxyRequest headers to the *http.Request
+	for key, value := range proxyRequest.Headers {
+		r.Header.Add(key, value)
+	}
+
+	// Create a x-url-formencoded string from QueryStringParameters
+	var queryString string
+	for key, value := range proxyRequest.QueryStringParameters {
+		queryString = fmt.Sprintf("%s%s=%s&", queryString, key, value)
+	}
+	queryString = strings.TrimSuffix(queryString, "&")
+
+	// Create a new url.URL
+	rURL := url.URL{
+		Scheme:     "https",
+		Opaque:     "",
+		User:       nil,
+		Host:       host,
+		Path:       proxyRequest.Path,
+		RawPath:    proxyRequest.Path,
+		ForceQuery: false,
+		RawQuery:   queryString,
+		Fragment:   "",
+	}
+
+	r.URL = &rURL
+
+	// Start serving
+	handler.ServeHTTP(w, r)
+
+	// Add the response headers to a map that can be used in the APIGatewayProxyResponse
+	// It is important to do this AFTER receiving requests
+	respHeaders := map[string]string{}
+
+	for key, value := range w.Header() {
+		respHeaders[key] = strings.Join(value, "")
+	}
+
+	proxyResp := events.APIGatewayProxyResponse{
+		Body:       w.Body.String(),
+		Headers:    respHeaders,
+		StatusCode: w.Code,
+	}
+	return proxyResp, nil
+}
